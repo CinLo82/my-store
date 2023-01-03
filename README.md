018af913cb2f20252f9e0ed7a4a049a113478680
CinLo <cinlos3382@gmail.com>
2023-01-03 09:32:59 -0300

    Separación de responsabilidades con express.Route

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Curso de Backend con Node.js: API REST con Express.js
+
 # my-store
+
+- Usa Node.js con Express.js para el backend de tu aplicación
+- Crea los endpoints de tu API REST
+- Crea tu primer servidor HTTP
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
 
 
 const app = express();
@@ -13,56 +13,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola , soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      imgen: faker.image.imageUrl(),
-    });
-
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  console.log('yo soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'product 2',
-    price: 2000
-  });
-});
-
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if(limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('no hay parametros');
-  }
-});
-
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-})
-
+routerApi(app);
 
 
 app.listen(port, () => {
