7016add6ca380c28f68d2c74fc685299fd1d0d2f
CinLo <cinlos3382@gmail.com>
2023-01-04 12:41:37 -0300

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("express-async-errors");
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -8,6 +9,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList = ['http://127.0.0.1:5500', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if ( whiteList.includes(origin) !== 1 ) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+
+app.use(cors( options ));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
