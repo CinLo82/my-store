88a4556ee942c6f39acdc6012400f6fd487f8974
CinLo <cinlos3382@gmail.com>
2023-01-09 08:49:27 -0300

    redireccionando las relaciones entre user y customer

--- routes/customers.router.js
+++ routes/customers.router.js
@@ -6,31 +6,24 @@ const { createCustomerSchema, updateCustomerSchema, getCustomerSchema } = requir
 const router = express.Router();
 const service = new CustomerService();
 
-router.get('/', async (req, res) => {
-  const customers = await service.find();
-  res.json(customers);
-  });
-
-  router.get('/:id',
-  validatorHandler(getCustomerSchema, 'params'),
-    async (req, res, next) => {
-      try {
-        const { id } = req.params;
-        const customer = await service.findOne(id);
-        res.json(customer);
-      } catch (error) {
-        next(error);
-      }
-    }
-);
+router.get('/',  async (req, res, next) => {
+  try {
+    res.json(await service.find());
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/',
   validatorHandler(createCustomerSchema, 'body'),
-    async (req, res) => {
-      const body = req.body;
-      const newUser = await service.create(body);
-      res.status(201).json(newUser);
+    async (req, res, next) => {
+      try{
+        const body = req.body;
+        res.status(201).json(await  service.create(body));
+    } catch (error) {
+      next(error);
     }
+  }
 );
 
 router.patch('/:id',
@@ -40,18 +33,23 @@ router.patch('/:id',
       try {
         const { id } = req.params;
         const body = req.body;
-        const customer = await service.update(id, body);
-        res.json(customer);
+        res.status(201).json(await service.update(id, body));
       } catch (error) {
         next(error);
       }
     }
 );
 
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const rta = await service.delete(id);
-  res.json(rta);
-});
+router.delete('/:id',
+  validatorHandler(getCustomerSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      res.status(200).json(await service.delete(id));
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
