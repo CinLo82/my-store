51bcbd1e61be3430aaf3ea403b79f654d492a5e2
CinLo <cinlos3382@gmail.com>
2023-01-11 08:07:56 -0300

    creando relacion muchos a muchos(order, product)

---
+++ db/models/order-product.model.js
@@ -0,0 +1,69 @@
+const { Model, DataTypes, Sequelize} = require('sequelize');
+const { ORDER_TABLE } = require('./order.model');
+const { PRODUCT_TABLE } = require('./product.model');
+
+const ORDER_PRODUCT_TABLE = 'orders_products';
+
+const OrderProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  createAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW
+  },
+  orderId: {
+    field: 'order_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: true,
+    references: {
+      model: ORDER_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+  amount: {
+    allowNull:false,
+    type: DataTypes.INTEGER,
+  },
+  productId: {
+    field: 'product_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: true,
+    references: {
+      model: PRODUCT_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  }
+}
+
+class OrderProduct extends Model {
+  static associate(models) {
+  //
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: ORDER_PRODUCT_TABLE,
+      modelName: 'OrderProduct',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = {
+  ORDER_PRODUCT_TABLE,
+  OrderProductSchema,
+  OrderProduct
+}
